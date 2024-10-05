24d98e8919f715b89dc1581f610bba532ab5cb30
li <li2604984003@gmail.com>
2024-10-05 14:33:03 +0800

    Initialization

--- src/test/java/com/li/bot/ProcessBotApplicationTests.java
+++ src/test/java/com/li/bot/ProcessBotApplicationTests.java
@@ -1,19 +1,16 @@
 package com.li.bot;
 
-import org.apache.commons.io.FileUtils;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.io.File;
 import java.io.IOException;
-import java.util.Date;
 
 @SpringBootTest
 class ProcessBotApplicationTests {
 
     @Test
     void contextLoads() throws IOException {
-
+        System.out.println();
     }
 
 }
